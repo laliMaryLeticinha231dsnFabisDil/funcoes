b8da3a66c73090b62c6dca9624ad23f82bd123f2
Letícia Fariasf 2b <133364942+laliMaryLeticinha231dsnFabisDil@users.noreply.github.com>
2024-05-24 14:53:41 -0300

    Initial commit

---
+++ funcoes/funcoes.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <locale>
+using namespace std;
+
+//PARA SOMAR 
+// 
+//int soma(int a, int b);
+	//essa função principal
+//int main() {
+//	int num1 = 5;
+//	int num2 = 3;
+//	int resultado = soma(5, 3);
+//	cout << resultado;
+//}
+//int soma(int a, int b) {
+//	return a + b;
+//}
+
+//void imprimeTexto ();
+//int main() {
+//	locale::global(locale("pt_BR.UTF-8"));
+//	imprimeTexto();
+//}
+//void imprimeTexto() {
+//	cout << "Essa funcao vazia, nao retorna nada";
+//}
+
+
+//int maior(int a, int b); //declarar função
+//int main() { //bloco de código principal
+//	locale::global(locale("pt_BR.UTF-8"));
+//	int num1, num2;
+//	cout << "Digite o primeiro número: ";
+//	cin >> num1;
+//	cout << "Digite o segundo número: ";
+//	cin >> num2;
+//	int resposta = maior(num1, num2);
+//	cout << "O maior entre eles é" << resposta;
+//}
+//int maior(int a, int b) { //função
+//	if (a > b) {
+//		return a;
+//	}
+//	else {
+//		return b;
+//	}
+//
+//}
+
+
+
+
+
+
+//      EXERCÍCIO 01
+
+
+int mult(int a, int b); //declarar função
+int main() { //bloco de código principal
+	locale::global(locale("pt_BR.UTF-8"));
+	int num1, num2;
+	cout << "Digite o valor da hora: ";
+	cin >> num1;
+	cout << "Digite horas trabalhadas: ";
+	cin >> num2;
+	int resposta = mult (num1, num2);
+	cout << "O maior entre eles é" << resposta;
+}
+int maior(int a, int b) { //função
+	if (a * b) {
+
+	}
+
+}
